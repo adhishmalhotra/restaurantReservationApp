965fb25e874f7c265b814641afc02408da7e07c6
Frederick Tetteh <frederick.tetteh@ontariotechu.net>
2022-11-16 17:45:29 -0500

    first commit

--- app/src/main/java/com/example/restaurantreservationapp/MainActivity.java
+++ app/src/main/java/com/example/restaurantreservationapp/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.restaurantreservationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button createbtn;
+    Button cancelbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cancelbtn = findViewById(R.id.cancelbutton);
+        createbtn =findViewById(R.id.createbutton);
+
+        createbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MakeAReservation.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
